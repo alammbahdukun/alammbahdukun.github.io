22042c91c2ccc2c013158a9471c2a06c162e33a3
Nourin Alam Adzani <108322974+alammbahdukun@users.noreply.github.com>
2022-06-28 13:57:02 +0700

    Add files via upload
    UAS PKW

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+var typed = new Typed('#typed', {
+      strings:['Nama Saya Nourin Alam Adzani.',
+               'Saya berasal dari Brebes.',
+               'Bermain Game, Mendengarkan Musik, dan Tidur adalah Hobi Saya .',
+               'Tekan icon di bawah ini jika Anda ingin mengunjungi media sosial saya.',
+               'Terima Kasih!',
+            ],
+      typeSpeed: 45,
+      backSpeed: 80,
+      fadeOut: true,
+      loop: true
+    });
+
+});
